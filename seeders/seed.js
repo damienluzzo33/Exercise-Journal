9c3f3ee18484d204d1d477f16744a9a5b960b35d
Damien Luzzo <damienluzzo33@gmail.com>
2021-11-18 16:45:23 -0600

    final touches and clean up

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Workouts = require('../models/workouts');
+require("dotenv").config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
